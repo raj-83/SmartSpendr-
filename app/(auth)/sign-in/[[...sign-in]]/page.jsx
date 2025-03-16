8e9004ff5376af3a1121c645bfb08274805b7eb3
raj-83 <rajeevranjanpusa2003@gmail.com>
2025-03-16 23:46:11 +0530

    Add initial application structure and components

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+import React from "react";
+
+const page = () =>{
+    return <SignIn/>
+    //header and footer are constant
+}
+
+export default page ;
---
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+import React from "react";
+
+const page = () =>{
+    return <SignUp/>
+    //header and footer are constant
+}
+
+export default page ;
