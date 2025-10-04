52ef569670b726dce9ae87b0763a7494952b6b46
raj-83 <rajeevranjanpusa2003@gmail.com>
2025-10-04 12:29:43 +0530

    Refactor code structure for improved readability and maintainability

--- app/lib/inngest/function.js
+++ app/lib/inngest/function.js
@@ -1,34 +1,35 @@
 import { db } from "@/lib/prisma";
 import { inngest } from "./client";
+import EmailTemplate from "@/emails/template";
 
 export const checkBudgetAlert = inngest.createFunction(
-  { name: "Check Budget Alerts" },
-  { cron: "0 */6 * * *" },
-  async ({ step }) => {
-     const budgets = await step.run("fetch-budget",async () =>{
-         
+    { name: "Check Budget Alerts" },
+    { cron: "0 */6 * * *" },
+    async ({ step }) => {
+        const budgets = await step.run("fetch-budget", async () => {
 
-        return await db.budget.findMany({
-             include:{
-                 user:{
-                    include:{
-                        accounts:{
-                            where:{
-                                isDefault:true,
+
+            return await db.budget.findMany({
+                include: {
+                    user: {
+                        include: {
+                            accounts: {
+                                where: {
+                                    isDefault: true,
+                                },
                             },
                         },
                     },
-                 },
-             },
+                },
+            });
         });
-     });
 
-        for(const budget of budgets){
-              
-            const defaultAccount =budget.user.accounts[0];
-            if(!defaultAccount) continue;
-            
-            await step.run(`check-budget-${budget.id}`,async()=>{
+        for (const budget of budgets) {
+
+            const defaultAccount = budget.user.accounts[0];
+            if (!defaultAccount) continue;
+
+            await step.run(`check-budget-${budget.id}`, async () => {
                 const currentDate = new Date();
                 const startOfMonth = new Date(
                     currentDate.getFullYear(),
@@ -37,58 +38,76 @@ export const checkBudgetAlert = inngest.createFunction(
                 );
                 const endOfMonth = new Date(
                     currentDate.getFullYear(),
-                    currentDate.getMonth()+1,
+                    currentDate.getMonth() + 1,
                     0
-                );    
+                );
 
 
                 const expenses = await db.transaction.aggregate({
-                    where:{
-                        userId:budget.userId,
-                        accountId:defaultAccount.id,
-                        type:"Expense",
-                        date:{
-                          gte: startOfMonth,
-                           lte: endOfMonth,
+                    where: {
+                        userId: budget.userId,
+                        accountId: defaultAccount.id,
+                        type: "Expense",
+                        date: {
+                            gte: startOfMonth,
+                            lte: endOfMonth,
                         },
                     },
-                    _sum:{
-                        amount:true,
+                    _sum: {
+                        amount: true,
                     },
                 });
 
 
-                 const totalExpenses = expenses._sum.amount?.toNumber() || 0;
-                 const budgetAmount = budget.amount.toNumber();
+                const totalExpenses = expenses._sum.amount?.toNumber() || 0;
+                const budgetAmount = budget.amount.toNumber();
 
-                 const percentageUsed = (totalExpenses / budgetAmount) * 100;
+                const percentageUsed = (totalExpenses / budgetAmount) * 100;
 
-                 if(percentageUsed >= 80 && (!budget.lastAlertSent || isNewMonth(new Date(budget.lastAlertSent),new Date()))){
+                if (percentageUsed >= 80 && (!budget.lastAlertSent || isNewMonth(new Date(budget.lastAlertSent), new Date()))) {
 
+
+                    //send alert
+
+                     await sendEmail({
+                          to: budget.user.email,
+                            subject: `Budget Alert for ${defaultAccount.name}`,
+                            react:EmailTemplate({
+                              
+                                userName: budget.user.name || "",
+                                type: "budget-alert",
+                                data: {
+                                    percentageUsed,
+                                    budgetAmount:parseInt(budgetAmount.toFixed(1)),
+                                    totalExpenses:parseInt( totalExpenses.toFixed(1)),
+                                    accountName:
+                                    defaultAccount.name,
+                            },
+                     }),
+                    });
                     
-                         //send alert
 
-                         //update last alert sent
-                          await db.budget.update({
+                    //update last alert sent
+                    await db.budget.update({
 
 
-                            where:{id:budget.id},
-                            data:{lastAlertSent:new Date()},
-                          });
-                 }
+                        where: { id: budget.id },
+                        data: { lastAlertSent: new Date() },
+                    });
+                }
 
             });
-            
+
         }
 
-  }
+    }
 );
 
- function isNewMonth(lastAlertDate, currentDate) {
-            
-    return(
-         lastAlertDate.getMonth() !== currentDate.getMonth() ||
-         lastAlertDate.getFullYear() !== currentDate.getFullYear()
+function isNewMonth(lastAlertDate, currentDate) {
+
+    return (
+        lastAlertDate.getMonth() !== currentDate.getMonth() ||
+        lastAlertDate.getFullYear() !== currentDate.getFullYear()
 
     )
- }
+}
